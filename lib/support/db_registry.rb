4b4ba5fcbdbc8960497beb7d99722bb6294c6396
Jordan Sleeper <ezirens@gmail.com>
2016-12-22 22:19:54 +0000

    Done.

--- lib/support/db_registry.rb
+++ lib/support/db_registry.rb
@@ -1,4 +1,6 @@
-DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/#{DBNAME}-test.db"), 
-  development: ConnectionAdapter.new("db/#{DBNAME}-development.db"), 
+require_relative 'connection_adapter.rb'
+
+DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/#{DBNAME}-test.db"),
+  development: ConnectionAdapter.new("db/#{DBNAME}-development.db"),
   production: ConnectionAdapter.new("db/#{DBNAME}-production.db")
   )
